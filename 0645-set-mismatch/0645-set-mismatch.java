4db26d242d60c4ee884ef58d0ad6028a882246ed
SARVAGYA BANSAL <81100088+Sarvagyaban@users.noreply.github.com>
2023-01-03 21:48:09 +0530

    Time: 594 ms (5.06%), Space: 43.5 MB (98.39%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -2,26 +2,24 @@ class Solution
 {
     public int[] findErrorNums(int[] nums) 
     {
-        int length=nums.length;
-        Arrays.sort(nums);
-        int duplicate=0;
-        for(int i=0;i<length;i++)
+    int b=0;
+    for (int i = 0; i < nums.length; i++)
+    {
+        for (int j = i+1; j < nums.length; j++)
         {
-            for(int j=i+1;j<length;j++)
-            
-            {
-                if(nums[i]==nums[j])
-                {
-                    duplicate=nums[i];
-                }
-            }
+            if(nums[i]==nums[j])
+            b=nums[i];
         }
-        int ans=1;
-        for(int m=0;m<length;m++){
-            if(nums[m]==ans){
-                ans++;
-            }
-        }
-        return new int[] {duplicate,ans};
     }
+    Arrays.sort(nums);
+    int ans=1;
+    int i=0;
+    for(i=0;i<=nums.length-1;i++)
+    {
+    if(nums[i]==ans)
+    {ans++;}
+    
+    }
+return new int[] {b,ans};  
+}
 }

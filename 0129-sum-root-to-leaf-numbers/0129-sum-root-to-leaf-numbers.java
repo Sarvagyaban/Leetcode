d1d285f0c9ec61a585284c83ff4432f2bab754fa
SARVAGYA BANSAL <81100088+Sarvagyaban@users.noreply.github.com>
2023-01-25 18:52:53 +0530

    Time: 0 ms (100.00%), Space: 42 MB (11.49%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -1,29 +1,20 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public int sumNumbers(TreeNode root) {
-        return func(root,0);
+ class Solution {
+    public int sumNumbers(TreeNode root) 
+    {
+     return sum(root,0);   
     }
-    public int func(TreeNode root,int res){
-        if(root==null){
+    public int sum(TreeNode root,int n)
+    {
+        if(root==null)
+        {
             return 0;
         }
-        if(root.right==null&&root.left==null){
-            return (res*10)+root.val;
+        if(root.left==null && root.right==null)
+        {
+            return n*10 + root.val;
         }
-        return (func(root.left,(res*10)+root.val)+func(root.right,(res*10)+root.val));
+        int ls=sum(root.left,n*10+root.val);
+        int rs=sum(root.right,n*10+root.val);
+        return ls+rs;
     }
 }

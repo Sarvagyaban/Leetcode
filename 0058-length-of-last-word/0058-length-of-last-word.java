8330dead8af413f68c9c142570b03d4607a331ad
SARVAGYA BANSAL <81100088+Sarvagyaban@users.noreply.github.com>
2023-03-16 22:37:01 +0530

    Time: 2 ms (13.37%), Space: 41.1 MB (17.13%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        int c=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i) ==' ')
+                c=0;
+            else
+                c++;
+        }
+        return c;
+    }
+}

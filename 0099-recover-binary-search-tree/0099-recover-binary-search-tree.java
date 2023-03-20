36ef07d82a9718d1392d0bbc8c2ef3e286bcf843
SARVAGYA BANSAL <81100088+Sarvagyaban@users.noreply.github.com>
2023-03-20 23:30:23 +0530

    Time: 2 ms (100.00%), Space: 42.8 MB (35.26%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -0,0 +1,24 @@
+public class Solution {
+    
+     TreeNode first = null, second = null, pre = null;
+    public void recoverTree(TreeNode root) {
+        if(root==null)return;
+        dfs(root);
+        if(first!=null&&second!=null){
+            int temp = first.val;
+            first.val = second.val;
+            second.val = temp;
+        }
+        
+    }
+    public void  dfs(TreeNode root){
+        if(root.left!=null)dfs(root.left);
+        if(pre!=null&&pre.val>root.val)
+            {
+                if(first==null) first = pre;
+                if(first!=null)second = root;
+            }
+        pre = root;
+        if(root.right!=null)dfs(root.right);
+    }
+}

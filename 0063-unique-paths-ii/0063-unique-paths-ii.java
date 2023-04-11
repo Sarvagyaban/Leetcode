a1424b4de1d0a7658fb92ee5b1ee13cf8c80a480
SARVAGYA BANSAL <81100088+Sarvagyaban@users.noreply.github.com>
2023-04-12 00:40:24 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (55.50%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,29 +1,38 @@
 class Solution {
- public int uniquePathsWithObstacles(int[][] obstacleGrid) {
- if(obstacleGrid[0][0]==1) return 0;
- int m = obstacleGrid.length;
- int n = obstacleGrid[0].length;
- for(int i=0;i<m;i++)
- {
- for(int j=0;j<n;j++)
- {
- if(i==0 && j==0)
- obstacleGrid[0][0]=1;
- else if(obstacleGrid[i][j]==1)
- obstacleGrid[i][j]=0;
- else if( i==0 && obstacleGrid[0][j-1]==0 )
- obstacleGrid[i][j]=0;
- else if( i==0 && obstacleGrid[0][j-1]==1 )
- obstacleGrid[i][j]=1;
- else if( j==0 && obstacleGrid[i-1][0]==0 )
- obstacleGrid[i][j]=0;
- else if( j==0 && obstacleGrid[i-1][0]==1 )
- obstacleGrid[i][j]=1;
- else
- obstacleGrid[i][j] = obstacleGrid[i][j-1] +
-obstacleGrid[i-1][j];
- }
- }
- return obstacleGrid[m-1][n-1];
- }
+    public int uniquePathsWithObstacles(int[][] arr1) {
+        int m=arr1.length;
+        int n=arr1[0].length;
+        int [][] arr =new int[m][n]; 
+        if(arr[0][0]==1) return 0;
+        for(int i=0;i<arr1[0].length;i++)
+        {
+         if(arr1[0][i]==1)
+            {break;}
+            else
+            {arr[0][i]=1;}
+        }
+        for(int i=0;i<arr1.length;i++)
+        {
+           if(arr1[i][0]==1)
+            {break;}
+            else
+            {arr[i][0]=1;}
+        }
+    
+       for(int i=1;i<arr.length;i++)
+        {
+            for(int j=1;j<arr[0].length;j++)
+            {
+                if(arr1[i][j]==1)
+                {
+                    arr[i][j]=0;
+                }
+                else
+                {
+                arr[i][j]=arr[i-1][j]+arr[i][j-1];
+            }}
+        }
+        return arr[m-1][n-1];
+        
+    }
 }

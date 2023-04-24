3c4d74bbe174b0e5ec9a58694e7cadede1d7c643
SARVAGYA BANSAL <81100088+Sarvagyaban@users.noreply.github.com>
2023-04-24 22:02:21 +0530

    Time: 1 ms (99.57%), Space: 43.7 MB (21.29%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int jump(int[] nums) 
+    {
+     int pos=0;
+        int des=0;
+        int jump=0;
+        
+        for(int i=0;i<nums.length-1;i++)
+        {
+            des=Math.max(des,nums[i]+i);
+                
+                if(pos==i){
+                    pos=des;
+                    jump++;
+                }
+        }
+                    return jump;
+
+    }
+}

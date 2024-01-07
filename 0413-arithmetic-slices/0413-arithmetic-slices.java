7d56f23d392b8abf6324274986fbe0cb6a38969e
SARVAGYA BANSAL <81100088+Sarvagyaban@users.noreply.github.com>
2024-01-07 21:25:42 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (12.99%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -1,20 +1,14 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) 
     {
-     int dp=0;
+     int dp[]=new int [nums.length];
         int res=0;
         for(int i=2;i<nums.length;i++)
         {
             if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2])
-            {
-                dp++;
-            }
-            else
-            {
-                res=res+(dp*(dp+1))/2;
-                dp=0;
-            }
+                dp[i]=dp[i-1]+1;
+            res+=dp[i];
         }
-        return res+(dp*(dp+1))/2;
+        return res;
     }
 }

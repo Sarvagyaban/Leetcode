9172a2070b580fb253227a747a4e2b17dc930c1a
SARVAGYA BANSAL <81100088+Sarvagyaban@users.noreply.github.com>
2024-01-10 20:43:04 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (7.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,6 +3,15 @@ class Solution {
     {
       if(n==0)return 0;
       if(n==1||n==2)return 1;
- return fib(n-1)+fib(n-2);
+        int total=0;
+        int a=1;
+        int b=1;
+        for(int i= 2;i<n;i++)
+        {
+            total=a+b;
+            a=b;
+            b=total;
+        }
+        return total;
     }  
     }

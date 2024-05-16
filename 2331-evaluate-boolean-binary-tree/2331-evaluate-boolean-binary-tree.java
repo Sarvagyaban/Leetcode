04cdfb44ac42bb5647d5959b5e089e438a40f11b
SARVAGYA BANSAL <81100088+Sarvagyaban@users.noreply.github.com>
2024-05-16 20:35:45 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (90.07%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
@@ -1,19 +1,16 @@
 class Solution {
-    public boolean evaluateTree(TreeNode root) {
-        if(root.left==null && root.right==null){
-            if(root.val==0){
-                return false;
-            }
-            else{
-                return true;
-            }
-        }
-        if(root.val==2){
-            return evaluateTree(root.left) || evaluateTree(root.right);
-        }
-        if(root.val==3){
-            return evaluateTree(root.left) && evaluateTree(root.right);
-        }
+    public boolean helper(TreeNode root) {
+        if(root.val == 0 || root.val == 1) {
+            return root.val == 1;
+        } else if(root.val == 2) {
+            return helper(root.left) || helper(root.right);
+        } else if(root.val == 3) {
+            return helper(root.left) && helper(root.right);
+        } 
+
         return false;
+    }
+    public boolean evaluateTree(TreeNode root) {
+        return helper(root);
     }
 }
